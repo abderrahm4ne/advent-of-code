c10b2968eb32bf534f6d78fb6f3dbae69bae7a3a
Your GitHub Username abderrahm4ne <abderrahmanebfed@gmail.com>
2024-12-21 11:33:51 +0100

    d

---
+++ day1/day1.js
@@ -0,0 +1,35 @@
+import fs from 'fs';
+
+function distance(path){
+
+
+const data = fs.readFileSync(path, 'utf-8');
+
+const lines = data.trim().split('\n');
+
+const left = [];
+const right = [];
+
+lines.forEach(line => {
+	const [l, r] = line.split("   ").map(Number);
+	left.push(l);
+	right.push(r);
+})
+
+left.sort((a, b) => a - b);
+right.sort((a, b) => a - b);
+
+	let totalDistance = 0;
+
+for (let i = 0; i < left.length; i++) {
+	totalDistance += Math.abs(left[i] - right[i]);
+}
+
+return totalDistance;
+}
+
+
+const path = './input.txt';
+
+const pairs = distance(path);
+console.log(pairs);
