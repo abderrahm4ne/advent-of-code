f568098fff1e815eeb24c3b7d223e2881df6acc3
Your GitHub Username abderrahm4ne <abderrahmanebfed@gmail.com>
2024-12-26 00:10:47 +0100

    c

---
+++ day3/day3Part1.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+
+function sumOfMul(path) {
+    let safeRep = 0;
+    const data = fs.readFileSync(path, 'utf-8');
+    const pattern = /mul\((\d{1,3}),(\d{1,3})\)/g;
+
+    let sum = 0;
+
+    for(let match of data.matchAll(pattern)){
+        const x = parseInt(match[1], 10);
+        const y = parseInt(match[2], 10);
+
+        sum += x * y;
+    }
+
+    return sum ;
+}
+
+const path = './day3/input.txt';
+let result = sumOfMul(path);
+console.log(result);
